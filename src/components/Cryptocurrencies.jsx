6629211b5e63cbcefccf8731af4bac5629f30674
Jordan <thememphiss@icloud.com>
2022-03-03 14:26:18 +0200

    init

---
+++ src/components/Cryptocurrencies.jsx
@@ -0,0 +1,23 @@
+import React, {useState}from 'react';
+import millify from 'millify';
+import {Link} from 'react-router-dom';
+import {Card, Row, Col, Input} from 'antd';
+
+import { useGetCryptosQuery } from '../services/cryptoApi';
+
+const Cryptocurrencies = () => {
+    const {data: cryptosList, isFetching} = useGetCryptosQuery();
+    const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
+
+    console.log(cryptos);
+
+    return (
+        <>
+            <Row>
+                
+            </Row>
+        </>
+    )
+}
+
+export default Cryptocurrencies
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': 'b3c0678360msha4809c0f74a0030p1f67d3jsnea62bfd397ce'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({baseUrl}),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest('/coins')
+        })
+    })
+});
+
+export const { useGetCryptosQuery } = cryptoApi;
